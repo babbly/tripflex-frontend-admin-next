a5bf1df01b8d2b67b38666bc2886a0539b2280bb
BaeJiHyeon <baebabbly@gmail.com>
2026-05-22 14:18:33 +0900

    더미데이터 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For local development, basePath is '/'
-  // This file will be overwritten during deployment with the appropriate basePath
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-    ],
-  },
   output: 'standalone',
 };
 
